igoropaniuk/u-boot

commit 2aa7f0fa510a7e4aa5ade687999a64ee3d0036a0

habv4: tools: Avoid hardcoded CSF size for SPL targets
Currently it's not possible to authenticate the U-Boot proper of
mx6ul_14x14_evk_defconfig target:

Authenticate image from DDR location 0x877fffc0...
bad magic magic=0x0 length=0x00 version=0x3
bad length magic=0x0 length=0x00 version=0x3
bad version magic=0x0 length=0x00 version=0x3
spl: ERROR:  image authentication fail

Commit 0633e134784a ("imx: hab: Increase CSF_SIZE for i.MX6 and
i.MX7 devices") has increased CSF_SIZE to avoid a possible issue
when booting encrypted boot images.

Commit d21bd69b6e95 ("tools: mkimage: add firmware-ivt image type
for HAB verification") is hardcoding the CSF and IVT sizes, the
new CSF size is not being considered and u-boot-ivt.img fails to
boot.

Avoid hardcoded CSF and IVT size to fix this issue.

Signed-off-by: Breno Lima <breno.lima@nxp.com>
Reviewed-by: Fabio Estevam <festevam@gmail.com>

common/image.c
@@ -60,6 +60,8 @@ static const image_header_t *image_get_ramdisk(ulong rd_addr, uint8_t arch,
 #endif /* !USE_HOSTCC*/
 
 #include <u-boot/crc.h>
+#include <imximage.h>
+#include <generated/autoconf.h>
 
 #ifndef CONFIG_SYS_BARGSIZE
 #define CONFIG_SYS_BARGSIZE 512
@@ -375,9 +377,9 @@ void image_print_contents(const void *ptr)
 		}
 	} else if (image_check_type(hdr, IH_TYPE_FIRMWARE_IVT)) {
 		printf("HAB Blocks:   0x%08x   0x0000   0x%08x\n",
-				image_get_load(hdr) - image_get_header_size(),
-				image_get_size(hdr) + image_get_header_size()
-						- 0x1FE0);
+			image_get_load(hdr) - image_get_header_size(),
+			(int)(image_get_size(hdr) + image_get_header_size()
+			+ sizeof(flash_header_v2_t) - CONFIG_CSF_SIZE));
 	}
 }
 

tools/default_image.c
@@ -19,6 +19,8 @@
 #include <image.h>
 #include <tee/optee.h>
 #include <u-boot/crc.h>
+#include <imximage.h>
+#include <generated/autoconf.h>
 
 static image_header_t header;
 
@@ -106,7 +108,8 @@ static void image_set_header(void *ptr, struct stat *sbuf, int ifd,
 
 	if (params->type == IH_TYPE_FIRMWARE_IVT)
 		/* Add size of CSF minus IVT */
-		imagesize = sbuf->st_size - sizeof(image_header_t) + 0x1FE0;
+		imagesize = sbuf->st_size - sizeof(image_header_t)
+			    + CONFIG_CSF_SIZE - sizeof(flash_header_v2_t);
 	else
 		imagesize = sbuf->st_size - sizeof(image_header_t);
 

commit 21ee65c7424878a17cddea57250fb2fbd32ea9cd

common: fix building image.c
Wrap image_decomp() and all dependent functions with
!ifdef(USE_HOSTCC) macro, as this function isn't used by
any tool from /tools.

Without this it leads to compilation issues (because of
CONFIG_LZMA from newly added #include <generated/autoconf.h>
statement, which pulls all these definitions):

In file included from tools/common/image.c:1:0:
./tools/../common/image.c: In function ‘image_decomp’:
./tools/../common/image.c:428:9: warning: implicit declaration of function
‘gunzip’; did you mean ‘munmap’? [-Wimplicit-function-declaration]
   ret = gunzip(load_buf, unc_len, image_buf, &image_len);
         ^~~~~~
         munmap
./tools/../common/image.c:450:3: error: unknown type name ‘SizeT’;
did you mean ‘size_t’?
   SizeT lzma_len = unc_len;
   ^~~~~
   size_t
./tools/../common/image.c:452:9: warning: implicit declaration of function
‘lzmaBuffToBuffDecompress’ [-Wimplicit-function-declaration]
   ret = lzmaBuffToBuffDecompress(load_buf, &lzma_len,
         ^~~~~~~~~~~~~~~~~~~~~~~~
scripts/Makefile.host:114: recipe for target 'tools/common/image.o' failed

Fixes: 2aa7f0fa51 ("habv4: tools: Avoid hardcoded CSF size for SPL target")
Signed-off-by: Igor Opaniuk <igor.opaniuk@toradex.com>

common/image-fit.c
@@ -1728,6 +1728,7 @@ int fit_conf_get_prop_node(const void *fit, int noffset,
 	return fit_conf_get_prop_node_index(fit, noffset, prop_name, 0);
 }
 
+#ifndef USE_HOSTCC
 static int fit_image_select(const void *fit, int rd_noffset, int verify)
 {
 	fit_image_print(fit, rd_noffset, "   ");
@@ -1743,6 +1744,7 @@ static int fit_image_select(const void *fit, int rd_noffset, int verify)
 
 	return 0;
 }
+#endif /* USE_HOSTCC */
 
 int fit_get_node_from_config(bootm_headers_t *images, const char *prop_name,
 			ulong addr)
@@ -1771,6 +1773,7 @@ int fit_get_node_from_config(bootm_headers_t *images, const char *prop_name,
 	return noffset;
 }
 
+#ifndef USE_HOSTCC
 /**
  * fit_get_image_type_property() - get property name for IH_TYPE_...
  *
@@ -2009,6 +2012,7 @@ int fit_image_load(bootm_headers_t *images, ulong addr,
 		} else {
 			loadbuf = map_sysmem(load, max_decomp_len);
 		}
+
 		if (image_decomp(comp, load, data, image_type,
 				loadbuf, buf, len, max_decomp_len, &load_end)) {
 			printf("Error decompressing %s\n", prop_name);
@@ -2060,7 +2064,6 @@ int boot_get_setup_fit(bootm_headers_t *images, uint8_t arch,
 	return ret;
 }
 
-#ifndef USE_HOSTCC
 int boot_get_fdt_fit(bootm_headers_t *images, ulong addr,
 		   const char **fit_unamep, const char **fit_uname_configp,
 		   int arch, ulong *datap, ulong *lenp)

common/image.c
@@ -383,6 +383,7 @@ void image_print_contents(const void *ptr)
 	}
 }
 
+#ifndef USE_HOSTCC
 /**
  * print_decomp_msg() - Print a suitable decompression/loading message
  *
@@ -483,8 +484,6 @@ int image_decomp(int comp, ulong load, ulong image_start, int type,
 	return ret;
 }
 
-
-#ifndef USE_HOSTCC
 #if CONFIG_IS_ENABLED(LEGACY_IMAGE_FORMAT)
 /**
  * image_get_ramdisk - get and verify ramdisk image