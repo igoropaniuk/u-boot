18343ac762a419f77e3cbe40169c601f24c0b83c
Sherry Sun <sherry.sun@nxp.com>
2019-08-19 18:55:54 -0400

    imx8mq: Add board_usb_init() and board_usb_cleanup()
    Add board_usb_init() and board_usb_cleanup() to do the basic
    power/tcpc/usbphy config work for imx8mq_evk.
    
    And set trustzone region 0 to allow both non-secure and secure access
    when trust zone is enabled in imx8m/soc.c. Since we found dwc3 USB
    controller fails to access DDR if the default region 0 is secure
    access only.
    
    Signed-off-by: Sherry Sun <sherry.sun@nxp.com>

--- arch/arm/mach-imx/imx8m/soc.c
+++ arch/arm/mach-imx/imx8m/soc.c
@@ -55,6 +55,13 @@ void enable_tzc380(void)
 	/* Enable TZASC and lock setting */
 	setbits_le32(&gpr->gpr[10], GPR_TZASC_EN);
 	setbits_le32(&gpr->gpr[10], GPR_TZASC_EN_LOCK);
+
+	/*
+	 * set Region 0 attribute to allow secure and non-secure read/write permission
+	 * Found some masters like usb dwc3 controllers can't work with secure memory.
+	 */
+	writel(0xf0000000, TZASC_BASE_ADDR + 0x108);
+
 }
 
 void set_wdog_reset(struct wdog_regs *wdog)
@@ -246,4 +253,59 @@ void reset_cpu(ulong addr)
 		 * spin for .5 seconds before reset
 		 */
 	}
+}
+
+#define FSL_SIP_GPC			0xC2000000
+#define FSL_SIP_CONFIG_GPC_PM_DOMAIN	0x03
+
+#ifdef CONFIG_SPL_BUILD
+static uint32_t gpc_pu_m_core_offset[11] = {
+	0xc00, 0xc40, 0xc80, 0xcc0,
+	0xdc0, 0xe00, 0xe40, 0xe80,
+	0xec0, 0xf00, 0xf40,
+};
+
+#define PGC_PCR				0
+
+void imx_gpc_set_m_core_pgc(unsigned int offset, bool pdn)
+{
+	uint32_t val;
+	uintptr_t reg = GPC_BASE_ADDR + offset;
+
+	val = readl(reg);
+	val &= ~(0x1 << PGC_PCR);
+
+	if(pdn)
+		val |= 0x1 << PGC_PCR;
+	writel(val, reg);
+}
+
+void imx8m_usb_power_domain(uint32_t domain_id, bool on)
+{
+	uint32_t val;
+	uintptr_t reg;
+
+	imx_gpc_set_m_core_pgc(gpc_pu_m_core_offset[domain_id], true);
+
+	reg = GPC_BASE_ADDR + (on ? 0xf8 : 0x104);
+	val = 1 << (domain_id > 3 ? (domain_id + 3) : domain_id);
+	writel(val, reg);
+	while (readl(reg) & val)
+		;
+	imx_gpc_set_m_core_pgc(gpc_pu_m_core_offset[domain_id], false);
+}
+#endif
+
+int imx8m_usb_power(int usb_id, bool on)
+{
+	if (usb_id > 1)
+		return -EINVAL;
+#ifdef CONFIG_SPL_BUILD
+	imx8m_usb_power_domain(2 + usb_id, on);
+#else
+	if (call_imx_sip(FSL_SIP_GPC, FSL_SIP_CONFIG_GPC_PM_DOMAIN,
+			 2 + usb_id, on))
+		return -EPERM;
+#endif
+	return 0;
 }
